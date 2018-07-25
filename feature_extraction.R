e6d734a9f5b8669df9f7a97376bb2114fb719118
古居 彬 <d185627@hiroshima-u.ac.jp>
2018-07-25 10:31:33 +0900

    add some fuctions in feature_extraction.R

--- feature_extraction.R
+++ feature_extraction.R
@@ -30,17 +30,17 @@ CalcSSC <- function(x, eps = 0.00005){
   ssc <- 0
   for (i in 2:(n-1)){
     if( (x[i] > x[i-1]) && (x[i] > x[i+1]) ){
-      if( (abs(x[i]-x[i+1]) >= eps)&&(abs(x[i]-x[i-1]) >= eps) ){
-        scc <- scc + 1
+      if( (abs(x[i]-x[i+1]) >= eps)||(abs(x[i]-x[i-1]) >= eps) ){
+        ssc <- ssc + 1
       }
     }
     if((x[i] < x[i-1]) && (x[i] < x[i+1]) ){
-      if( (abs(x[i]-x[i+1]) >= eps)&&(abs(x[i]-x[i-1]) >= eps) ){
-        scc <- scc + 1
+      if( (abs(x[i]-x[i+1]) >= eps)||(abs(x[i]-x[i-1]) >= eps) ){
+        ssc <- ssc + 1
       }
     }
   }
-  return(scc)
+  return(ssc)
 }
 
 
@@ -52,4 +52,38 @@ CalcWL <- function(x){
     WL <- WL + delta
   }
   return(WL)
+}
+
+
+splitWithOverlap <- function(vec, seg.length, overlap) {
+  starts = seq(1, length(vec), by=seg.length-overlap)
+  ends   = starts + seg.length - 1
+  ends[ends > length(vec)] = length(vec)
+  
+  lapply(1:length(starts), function(i) vec[starts[i]:ends[i]])
+}
+
+GetHudginsFeatureSet <- function(x, samplingFreq, windowTime = 0.25, overlapTime = 0.125){
+  nrows <- NROW(x)
+  
+  vec <- seq(1,nrows)
+  
+  seg.length <- samplingFreq*windowTime
+  overlap <- samplingFreq*overlapTime
+  
+  segIndex <- splitWithOverlap(vec, seg.length, overlap)
+  
+  features <- matrix(0, nrow = length(segIndex), ncol = 12)
+  
+  for (i in 1:length(segIndex)){
+    MAV <- apply(x[segIndex[[i]],], 2, CalcMAV)
+    SSC <- apply(x[segIndex[[i]],], 2, CalcSSC)
+    WL <- apply(x[segIndex[[i]],], 2, CalcWL)
+    ZC <- apply(x[segIndex[[i]],], 2, CalcZC)
+    featureSet <- cbind(MAV, SSC, WL, ZC)
+    dim(featureSet) <- c(12,1)
+    features[i,] <- featureSet
+  }
+  
+  return(features)
 }
